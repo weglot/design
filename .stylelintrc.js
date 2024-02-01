4ac728a0379790b331adac59453cc88e6b816dca
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-02-01 17:05:58 +0100

    Fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,6 +8,7 @@ module.exports = {
             "ignoreFontFamilies": ["Inter", "Material Icons"],
         }],
         "no-descending-specificity": null, // TODO
+        "scss/at-import-partial-extension": null, // Cannot build with this rule
         "scss/comment-no-empty": null, // Lot of empty comment in boostrap reboot file
         "scss/no-global-function-names": null, // May need to update some build config
     }
