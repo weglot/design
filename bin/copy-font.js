bb1aaa8469385fa3386746642d70f6c43113045d
David TRINH <118731144+dtrinh-weglot@users.noreply.github.com>
2024-10-22 17:51:46 +0200

    Quality/Improve gitignore by adding dist and docs directories (#72)

--- bin/copy-font.js
+++ bin/copy-font.js
@@ -2,12 +2,30 @@
 // in order to avoid 404 when serving documentation.
 
 const fs = require('fs');
-fs.copyFile('node_modules/material-icons/iconfont/material-icons.woff', 'docs/assets/css/material-icons.woff', (err) => {
-    if (err) throw err;
-    console.log('File material-icons.woff was copied to destination');
-});
+const path = require('path');
 
-fs.copyFile('node_modules/material-icons/iconfont/material-icons.woff2', 'docs/assets/css/material-icons.woff2', (err) => {
-    if (err) throw err;
-    console.log('File material-icons.woff2 was copied to destination');
-});
+function ensureDirExistence(filePath) {
+    const dirname = path.dirname(filePath);
+    if (!fs.existsSync(dirname)) {
+        fs.mkdirSync(dirname, {recursive: true}); // recursive option creates parent directories if needed
+    }
+}
+
+ensureDirExistence('docs/assets/css/material-icons.woff');
+fs.copyFile(
+    'node_modules/material-icons/iconfont/material-icons.woff',
+    'docs/assets/css/material-icons.woff',
+    (err) => {
+        if (err) throw err;
+        console.log('File material-icons.woff was copied to destination');
+    }
+);
+
+fs.copyFile(
+    'node_modules/material-icons/iconfont/material-icons.woff2',
+    'docs/assets/css/material-icons.woff2',
+    (err) => {
+        if (err) throw err;
+        console.log('File material-icons.woff2 was copied to destination');
+    }
+);
