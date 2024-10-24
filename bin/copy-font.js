e64d736902c5f32ff686eb21d974bff8475fe4e2
Floran Pagliai <floran@weglot.com>
2024-10-24 12:53:59 +0200

    Github pages publish workflow

--- bin/copy-font.js
+++ bin/copy-font.js
@@ -1,31 +1,34 @@
-// The purpose of this script is to copy fonts from material-icons npm passage to jekyll build directory
-// in order to avoid 404 when serving documentation.
-
 const fs = require('fs');
 const path = require('path');
+
+// Get basePath from command-line arguments or default to 'docs'
+const basePath = process.argv[2] || 'docs/';
 
 function ensureDirExistence(filePath) {
     const dirname = path.dirname(filePath);
     if (!fs.existsSync(dirname)) {
-        fs.mkdirSync(dirname, {recursive: true}); // recursive option creates parent directories if needed
+        fs.mkdirSync(dirname, { recursive: true });
     }
 }
 
-ensureDirExistence('docs/assets/css/material-icons.woff');
+const destinationWoff = path.join(basePath, 'assets/css/material-icons.woff');
+const destinationWoff2 = path.join(basePath, 'assets/css/material-icons.woff2');
+// Ensure the directory exists and copy the files
+ensureDirExistence(destinationWoff);
 fs.copyFile(
     'node_modules/material-icons/iconfont/material-icons.woff',
-    'docs/assets/css/material-icons.woff',
+    destinationWoff,
     (err) => {
         if (err) throw err;
-        console.log('File material-icons.woff was copied to destination');
+        console.log(`File material-icons.woff was copied to ${destinationWoff}`);
     }
 );
 
 fs.copyFile(
     'node_modules/material-icons/iconfont/material-icons.woff2',
-    'docs/assets/css/material-icons.woff2',
+    destinationWoff2,
     (err) => {
         if (err) throw err;
-        console.log('File material-icons.woff2 was copied to destination');
+        console.log(`File material-icons.woff2 was copied to ${destinationWoff2}`);
     }
 );
